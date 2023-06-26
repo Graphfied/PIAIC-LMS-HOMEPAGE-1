8dbebd7dff2404b1e06599d4bc9bc39acd71d4f8
Graphfied AKA Fahad <90051783+Graphfied@users.noreply.github.com>
2023-06-26 13:45:15 +0500

    Add files via upload

---
+++ src/screens/PiaicLmsFirstPage/PiaicLmsFirstPage.jsx
@@ -0,0 +1,231 @@
+import React from "react";
+import "./style.css";
+
+export const PiaicLmsFirstPage = () => {
+  return (
+    <div className="PIAIC-LMS-first-page">
+      <div className="div">
+        <div className="overlap">
+          <div className="overlap-group">
+            <div className="overlap-2">
+              <div className="ellipse" />
+              <img className="herobg" alt="Herobg" src="/img/herobg-1.png" />
+            </div>
+            <div className="overlap-3">
+              <div className="ellipse-2" />
+              <img
+                className="launch-your-dev"
+                alt="Launch your dev"
+                src="/img/launch-your-dev-career-with-project-based-coaching.png"
+              />
+              <p className="showcase-your-skills">
+                Showcase your skills with practical development experience
+                <br />
+                and land the coding career of your dreams
+              </p>
+              <div className="navbar">
+                <div className="frame">
+                  <div className="overlap-group-2">
+                    <div className="text-wrapper">Courses</div>
+                    <div className="span">
+                      <div className="image" />
+                      <img className="img" alt="Image" src="/img/image.png" />
+                    </div>
+                  </div>
+                </div>
+                <div className="text-wrapper-2">Masterclass Experience</div>
+                <div className="text-wrapper-3">More</div>
+                <div className="image-wrapper">
+                  <div className="image-2" />
+                </div>
+                <div className="div-wrapper">
+                  <div className="text-wrapper-4">EDUMATE</div>
+                </div>
+              </div>
+              <img className="image-3" alt="Image" src="/img/image.png" />
+            </div>
+            <img className="welcome-to-edumate" alt="Welcome to edumate" src="/img/welcome-to-edumate.png" />
+          </div>
+          <div className="overlap-4">
+            <div className="text-wrapper-5">Explore Courses</div>
+          </div>
+        </div>
+        <div className="overlap-5">
+          <div className="overlap-6">
+            <div className="ellipse-3" />
+            <p className="p">Will I receive a certificate for each course?</p>
+            <p className="text-wrapper-6">Can I join the masterclass if I purchase a course?</p>
+            <p className="text-wrapper-7">How long does each course last?</p>
+            <img className="line" alt="Line" src="/img/line-4.svg" />
+            <img className="line-2" alt="Line" src="/img/line-4.svg" />
+          </div>
+          <h1 className="h-1">Frequently Asked Questions</h1>
+          <div className="overlap-7">
+            <div className="text-wrapper-8">+</div>
+            <div className="text-wrapper-9">+</div>
+          </div>
+          <div className="text-wrapper-10">+</div>
+          <div className="text-wrapper-11">+</div>
+        </div>
+        <div className="overlap-8">
+          <div className="ellipse-4" />
+          <div className="div-flex">
+            <div className="div-w-full">
+              <p className="heading-get-the">Get the 2023 Front-End Web Developer Roadmap</p>
+              <div className="p-font-poppins">
+                <p className="this-roadmap-will">
+                  This roadmap will provide you with a step-by-step journey and project ideas
+                  <br />
+                  that will accelerate your career.
+                </p>
+              </div>
+              <div className="div-mt">
+                <div className="input-input">
+                  <div className="div-placeholder">
+                    <div className="text-wrapper-12">Your Email</div>
+                  </div>
+                </div>
+                <div className="div-flex-button">
+                  <div className="button-subscribe">Subscribe</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="div-flex-2">
+            <div className="div-flex-3">
+              <div className="text-wrapper-13">Courses</div>
+              <div className="div-mt-2">
+                <div className="text-wrapper-14">Filmpire</div>
+                <div className="text-wrapper-15">Cryptoket</div>
+                <div className="text-wrapper-16">Javascript</div>
+                <div className="text-wrapper-17">Bootcamp</div>
+              </div>
+            </div>
+            <div className="div-flex-4">
+              <div className="text-wrapper-18">EDUMATE</div>
+              <div className="div-mt-3">
+                <div className="text-wrapper-19">Our Story</div>
+                <div className="text-wrapper-20">Careers at JSM</div>
+                <div className="text-wrapper-21">Contact Us</div>
+                <div className="text-wrapper-22">Terms of Use</div>
+                <div className="text-wrapper-23">Privacy Policy</div>
+              </div>
+            </div>
+            <div className="div-flex-5">
+              <div className="text-wrapper-24">Resources</div>
+              <div className="div-mt-4">
+                <div className="text-wrapper-25">Knowledge Base</div>
+                <div className="text-wrapper-26">YouTube Videos</div>
+                <div className="text-wrapper-27">Free Guides</div>
+                <div className="text-wrapper-28">Useful Links</div>
+              </div>
+            </div>
+          </div>
+          <img className="line-3" alt="Line" src="/img/line-5.svg" />
+          <img className="line-4" alt="Line" src="/img/line-6.svg" />
+        </div>
+        <img className="ellipse-5" alt="Ellipse" src="/img/ellipse-1.png" />
+        <p className="hands-on-projects">
+          Hands-on projects are the most practical way to learn a programming language <br />
+          and build your portfolio. If you&#39;re tired of building &#34;to-do&#34; apps and learning theory,
+          <br />
+          JS Mastery Pro can help you solidify your knowledge and start taking on
+          <br />
+          meaningful projects that will set the tone for your career.
+        </p>
+        <div className="overlap-9">
+          <div className="text-wrapper-29">Video / Content Here</div>
+        </div>
+        <img
+          className="comprehensive"
+          alt="Comprehensive"
+          src="/img/comprehensive-project-based-courses-to-help-you-become-a-javascr.png"
+        />
+        <p className="want-to-master-react">
+          Want to master React.js or create a stunning Web 3.0 blockchain app? Join other professional
+          <br />
+          developers inside Filmpire where you&#39;ll build a Netflix clone streaming app or NFT Marketplace <br />
+          where you&#39;ll develop Cryptoket, a fully-functioning NFT platform.
+        </p>
+        <div className="rectangle" />
+        <div className="rectangle-2" />
+        <div className="text-wrapper-30">Excellent</div>
+        <img className="stars" alt="Stars" src="/img/stars-5-1.svg" />
+        <p className="element-reviews-on">1,642 reviews on&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Trustpilot</p>
+        <div className="frame-wrapper">
+          <div className="frame-2">
+            <p className="text-wrapper-31">EDUMATE IS PROUD TO INTRODUCE</p>
+            <div className="the-EDUMATE-pro-wrapper">
+              <img
+                className="the-EDUMATE-pro"
+                alt="The EDUMATE pro"
+                src="/img/the-edumate-pro-masterclass-experience.png"
+              />
+            </div>
+            <p className="showcase-your">
+              Showcase your expertise to employers with exciting projects where you&#39;ll <br />
+              build real-world applications and have your code reviewed by industry <br />
+              experts.
+              <br />
+              Learn how to position yourself for a rewarding career with weekly live Q&amp;As, <br />
+              mock interviews, LinkedIn and resume/CV guidance, and more.
+            </p>
+            <div className="overlap-10">
+              <div className="rectangle-button" />
+              <div className="learn-more-button">Learn More</div>
+            </div>
+          </div>
+        </div>
+        <img
+          className="ditch-theory-driven"
+          alt="Ditch theory driven"
+          src="/img/ditch-theory-driven-courses-and-enter-the-world-of-a-true-dev.png"
+        />
+        <div className="chat-wrapper">
+          <img className="chat" alt="Chat" src="/img/chat-c2f8cd80-1.svg" />
+        </div>
+        <div className="medal-wrapper">
+          <img className="medal" alt="Medal" src="/img/medal-3c87d360-1.svg" />
+        </div>
+        <div className="computer-wrapper">
+          <img className="computer" alt="Computer" src="/img/computer-ede2bb96-1.svg" />
+        </div>
+        <div className="text-wrapper-32">Expert Coaching Support</div>
+        <div className="text-wrapper-33">Real-world experience</div>
+        <div className="text-wrapper-34">EDUMATE Certificates</div>
+        <p className="gain-access-to">
+          Gain 24/7 access to a private <br />
+          community of coders looking to <br />
+          advance their knowledge and <br />
+          experience, as well as their <br />
+          careers.
+        </p>
+        <p className="theory-will-only-get">
+          Theory will only get you so far, <br />
+          but building projects from the <br />
+          ground up will bolster your <br />
+          career in the software <br />
+          development space.
+        </p>
+        <p className="earn-JSM-pro">
+          Earn JSM Pro certification for <br />
+          each course you complete and <br />
+          show hiring managers and <br />
+          clients that you&#39;re a <br />
+          programming pro.
+        </p>
+        <p className="text-wrapper-35">How does the masterclass work?</p>
+        <div className="text-wrapper-36">+</div>
+        <img className="line-5" alt="Line" src="/img/line-4.svg" />
+        <img className="line-6" alt="Line" src="/img/line-4.svg" />
+        <p className="text-wrapper-37">Copyright © 2023 EDUMATE | All Rights Reserved</p>
+        <img className="discord-footer" alt="Discord footer" src="/img/discord-footer-c3f9bec4-1.svg" />
+        <img className="twitter-footer" alt="Twitter footer" src="/img/twitter-footer-fb4598b5-1.svg" />
+        <img className="youtube-footer" alt="Youtube footer" src="/img/youtube-footer-a6a0f876-1.svg" />
+        <img className="instagram-footer" alt="Instagram footer" src="/img/instagram-footer-9635b9bd-1.svg" />
+        <img className="linkedin-footer" alt="Linkedin footer" src="/img/linkedin-footer-e2dadf34-1.svg" />
+        <img className="github-footer" alt="Github footer" src="/img/github-footer-a3ddacc1-1.svg" />
+      </div>
+    </div>
+  );
+};
---
+++ src/screens/PiaicLmsFirstPage/index.js
@@ -0,0 +1,1 @@
+export { PiaicLmsFirstPage } from "./PiaicLmsFirstPage";
